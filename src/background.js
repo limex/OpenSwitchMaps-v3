d95e3babf6560528f47a53d36d5cac55e10371f6
limex <guebosch@gmail.com>
2025-02-22 19:40:56 +0100

    blue icon: enabled, red: disabled

--- src/background.js
+++ src/background.js
@@ -1,11 +1,18 @@
 const { isMatchingAMap } = require('./maps');
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (isMatchingAMap(tab.url)) {
-      // Enable extension icon for the tab that matches a map
-      chrome.action.enable(tabId);
-    } else {
-      // Disable extension icon for the tab that does not match a map
-      chrome.action.disable(tabId);
+    if (changeInfo.url) {
+        const isMatch = isMatchingAMap(changeInfo.url);
+        console.log('URL:', changeInfo.url,' -> isMatchingAMap:', isMatch);
+        if (isMatch) {
+            // Enable extension icon for the tab that matches a map
+            chrome.action.enable(tabId);
+            chrome.action.setIcon({ tabId, path: 'icon128.png' });
+        } else {
+            // Disable extension icon for the tab that does not match a map
+            chrome.action.disable(tabId);
+            // Change the extension icon for the tab that does not match a map
+            chrome.action.setIcon({ tabId, path: 'icon128-no.png' });
+        }
     }
 });
