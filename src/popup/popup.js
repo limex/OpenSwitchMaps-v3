c7c746219fd680cf5d25e1d5cd235aa28049e029
limex <guebosch@gmail.com>
2026-04-13 21:05:52 +0200

    optimise

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,4 @@
-import { filter, groupBy } from 'lodash-es';
 const Vue = require('vue').default;
 const Popup = require('./Popup.vue');
 
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  new Vue(Popup).$mount('#popup');
-});
+new Vue(Popup).$mount('#popup');
